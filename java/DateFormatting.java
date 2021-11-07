57f23a45f45a28327d5eb8d0e114923e68d18642
Val-Resh <80176765+Val-Resh@users.noreply.github.com>
2021-11-07 13:35:17 +0000

    improve code

--- java/DateFormatting.java
+++ java/DateFormatting.java
@@ -2,9 +2,9 @@ import java.time.ZoneId;
 import java.time.LocalDateTime;
 
 public class DateFormatting {
-    private static final ZoneId zoneId = ZoneId.systemDefault();
-    private static LocalDateTime today = LocalDateTime.now();
-    private static LocalDateTime nextMonth = today.plusMonths(1);
-    public static long startDate = today.atZone(zoneId).toEpochSecond();
-    public static long endDate = nextMonth.atZone(zoneId).toEpochSecond();
+    private final ZoneId zoneId = ZoneId.systemDefault();
+    private LocalDateTime today = LocalDateTime.now();
+    private LocalDateTime nextMonth = today.plusMonths(1);
+    public long startDate = today.atZone(zoneId).toEpochSecond();
+    public long endDate = nextMonth.atZone(zoneId).toEpochSecond();
 }
